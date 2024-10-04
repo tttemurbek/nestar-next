bcffa57ca5b79afc775c468dfbc4ab66419898e0
tttemurbek <nonemenoneme9999@gmail.com>
2024-10-04 15:35:59 +0900

    feat: develop agents list screenComponent logic

--- libs/components/common/AgentCard.tsx
+++ libs/components/common/AgentCard.tsx
@@ -11,72 +11,73 @@ import { useReactiveVar } from '@apollo/client';
 import { userVar } from '../../../apollo/store';
 
 interface AgentCardProps {
-	agent: any;
+  agent: any;
+  likeMemberHandler: any;
 }
 
 const AgentCard = (props: AgentCardProps) => {
-	const { agent } = props;
-	const device = useDeviceDetect();
-	const user = useReactiveVar(userVar);
-	const imagePath: string = agent?.memberImage
-		? `${REACT_APP_API_URL}/${agent?.memberImage}`
-		: '/img/profile/defaultUser.svg';
+  const { agent, likeMemberHandler } = props;
+  const device = useDeviceDetect();
+  const user = useReactiveVar(userVar);
+  const imagePath: string = agent?.memberImage
+    ? `${REACT_APP_API_URL}/${agent?.memberImage}`
+    : '/img/profile/defaultUser.svg';
 
-	if (device === 'mobile') {
-		return <div>AGENT CARD</div>;
-	} else {
-		return (
-			<Stack className="agent-general-card">
-				<Link
-					href={{
-						pathname: '/agent/detail',
-						query: { agentId: agent?._id },
-					}}
-				>
-					<Box
-						component={'div'}
-						className={'agent-img'}
-						style={{
-							backgroundImage: `url(${imagePath})`,
-							backgroundSize: 'cover',
-							backgroundPosition: 'center',
-							backgroundRepeat: 'no-repeat',
-						}}
-					>
-						<div>{agent?.memberProperties} properties</div>
-					</Box>
-				</Link>
+  if (device === 'mobile') {
+    return <div>AGENT CARD</div>;
+  } else {
+    return (
+      <Stack className="agent-general-card">
+        <Link
+          href={{
+            pathname: '/agent/detail',
+            query: { agentId: agent?._id },
+          }}
+        >
+          <Box
+            component={'div'}
+            className={'agent-img'}
+            style={{
+              backgroundImage: `url(${imagePath})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+            }}
+          >
+            <div>{agent?.memberProperties} properties</div>
+          </Box>
+        </Link>
 
-				<Stack className={'agent-desc'}>
-					<Box component={'div'} className={'agent-info'}>
-						<Link
-							href={{
-								pathname: '/agent/detail',
-								query: { agentId: 'id' },
-							}}
-						>
-							<strong>{agent?.memberFullName ?? agent?.memberNick}</strong>
-						</Link>
-						<span>Agent</span>
-					</Box>
-					<Box component={'div'} className={'buttons'}>
-						<IconButton color={'default'}>
-							<RemoveRedEyeIcon />
-						</IconButton>
-						<Typography className="view-cnt">{agent?.memberViews}</Typography>
-						<IconButton color={'default'}>
-							{agent?.meLiked && agent?.meLiked[0]?.myFavorite ? (
-								<FavoriteIcon color={'primary'} />
-							) : (
-								<FavoriteBorderIcon />
-							)}
-						</IconButton>
-						<Typography className="view-cnt">{agent?.memberLikes}</Typography>
-					</Box>
-				</Stack>
-			</Stack>
-		);
-	}
+        <Stack className={'agent-desc'}>
+          <Box component={'div'} className={'agent-info'}>
+            <Link
+              href={{
+                pathname: '/agent/detail',
+                query: { agentId: 'id' },
+              }}
+            >
+              <strong>{agent?.memberFullName ?? agent?.memberNick}</strong>
+            </Link>
+            <span>Agent</span>
+          </Box>
+          <Box component={'div'} className={'buttons'}>
+            <IconButton color={'default'}>
+              <RemoveRedEyeIcon />
+            </IconButton>
+            <Typography className="view-cnt">{agent?.memberViews}</Typography>
+            <IconButton color={'default'} onClick={() => likeMemberHandler(user, agent?._id)}>
+              {agent?.meLiked && agent?.meLiked[0]?.myFavorite ? (
+                <FavoriteIcon color={'primary'} />
+              ) : (
+                <FavoriteBorderIcon />
+              )}
+            </IconButton>
+            <Typography className="view-cnt">{agent?.memberLikes}</Typography>
+          </Box>
+        </Stack>
+      </Stack>
+    );
+  }
 };
 
 export default AgentCard;
--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -9,175 +9,216 @@ import AgentCard from '../../libs/components/common/AgentCard';
 import { useRouter } from 'next/router';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { Member } from '../../libs/types/member/member';
+import { GET_AGENTS } from '../../apollo/user/query';
+import { useQuery, useMutation } from '@apollo/client';
+import { T } from '../../libs/types/common';
+import { LIKE_TARGET_MEMBER } from '../../apollo/user/mutation';
+import { Messages } from '../../libs/config';
+import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
 
 export const getStaticProps = async ({ locale }: any) => ({
-	props: {
-		...(await serverSideTranslations(locale, ['common'])),
-	},
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
 });
 
 const AgentList: NextPage = ({ initialInput, ...props }: any) => {
-	const device = useDeviceDetect();
-	const router = useRouter();
-	const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
-	const [filterSortName, setFilterSortName] = useState('Recent');
-	const [sortingOpen, setSortingOpen] = useState(false);
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const [searchFilter, setSearchFilter] = useState<any>(
-		router?.query?.input ? JSON.parse(router?.query?.input as string) : initialInput,
-	);
-	const [agents, setAgents] = useState<Member[]>([]);
-	const [total, setTotal] = useState<number>(0);
-	const [currentPage, setCurrentPage] = useState<number>(1);
-	const [searchText, setSearchText] = useState<string>('');
+  const device = useDeviceDetect();
+  const router = useRouter();
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
+  const [filterSortName, setFilterSortName] = useState('Recent');
+  const [sortingOpen, setSortingOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [searchFilter, setSearchFilter] = useState<any>(
+    router?.query?.input ? JSON.parse(router?.query?.input as string) : initialInput,
+  );
+  const [agents, setAgents] = useState<Member[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchText, setSearchText] = useState<string>('');
 
-	/** APOLLO REQUESTS **/
-	/** LIFECYCLES **/
-	useEffect(() => {
-		if (router.query.input) {
-			const input_obj = JSON.parse(router?.query?.input as string);
-			setSearchFilter(input_obj);
-		} else
-			router.replace(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`);
+  /** APOLLO REQUESTS **/
+  const [likeTargetMember] = useMutation(LIKE_TARGET_MEMBER);
 
-		setCurrentPage(searchFilter.page === undefined ? 1 : searchFilter.page);
-	}, [router]);
+  const {
+    loading: getAgentsLoading,
+    data: getAgentsData,
+    error: getAgentsError,
+    refetch: getAgentsRefetch,
+  } = useQuery(GET_AGENTS, {
+    fetchPolicy: 'network-only',
+    variables: { input: searchFilter },
+    notifyOnNetworkStatusChange: true,
+    onCompleted: (data: T) => {
+      setAgents(data?.getAgents?.list);
+      setTotal(data?.getAgents?.metaCounter[0]?.total);
+    },
+  });
 
-	/** HANDLERS **/
-	const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
-		setAnchorEl(e.currentTarget);
-		setSortingOpen(true);
-	};
+  /** LIFECYCLES **/
+  useEffect(() => {
+    if (router.query.input) {
+      const input_obj = JSON.parse(router?.query?.input as string);
+      setSearchFilter(input_obj);
+    } else
+      router.replace(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`);
 
-	const sortingCloseHandler = () => {
-		setSortingOpen(false);
-		setAnchorEl(null);
-	};
+    setCurrentPage(searchFilter.page === undefined ? 1 : searchFilter.page);
+  }, [router]);
 
-	const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
-		switch (e.currentTarget.id) {
-			case 'recent':
-				setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'DESC' });
-				setFilterSortName('Recent');
-				break;
-			case 'old':
-				setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'ASC' });
-				setFilterSortName('Oldest order');
-				break;
-			case 'likes':
-				setSearchFilter({ ...searchFilter, sort: 'memberLikes', direction: 'DESC' });
-				setFilterSortName('Likes');
-				break;
-			case 'views':
-				setSearchFilter({ ...searchFilter, sort: 'memberViews', direction: 'DESC' });
-				setFilterSortName('Views');
-				break;
-		}
-		setSortingOpen(false);
-		setAnchorEl2(null);
-	};
+  /** HANDLERS **/
+  const sortingClickHandler = (e: MouseEvent<HTMLElement>) => {
+    setAnchorEl(e.currentTarget);
+    setSortingOpen(true);
+  };
 
-	const paginationChangeHandler = async (event: ChangeEvent<unknown>, value: number) => {
-		searchFilter.page = value;
-		await router.push(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`, {
-			scroll: false,
-		});
-		setCurrentPage(value);
-	};
+  const sortingCloseHandler = () => {
+    setSortingOpen(false);
+    setAnchorEl(null);
+  };
 
-	if (device === 'mobile') {
-		return <h1>AGENTS PAGE MOBILE</h1>;
-	} else {
-		return (
-			<Stack className={'agent-list-page'}>
-				<Stack className={'container'}>
-					<Stack className={'filter'}>
-						<Box component={'div'} className={'left'}>
-							<input
-								type="text"
-								placeholder={'Search for an agent'}
-								value={searchText}
-								onChange={(e: any) => setSearchText(e.target.value)}
-								onKeyDown={(event: any) => {
-									if (event.key == 'Enter') {
-										setSearchFilter({
-											...searchFilter,
-											search: { ...searchFilter.search, text: searchText },
-										});
-									}
-								}}
-							/>
-						</Box>
-						<Box component={'div'} className={'right'}>
-							<span>Sort by</span>
-							<div>
-								<Button onClick={sortingClickHandler} endIcon={<KeyboardArrowDownRoundedIcon />}>
-									{filterSortName}
-								</Button>
-								<Menu anchorEl={anchorEl} open={sortingOpen} onClose={sortingCloseHandler} sx={{ paddingTop: '5px' }}>
-									<MenuItem onClick={sortingHandler} id={'recent'} disableRipple>
-										Recent
-									</MenuItem>
-									<MenuItem onClick={sortingHandler} id={'old'} disableRipple>
-										Oldest
-									</MenuItem>
-									<MenuItem onClick={sortingHandler} id={'likes'} disableRipple>
-										Likes
-									</MenuItem>
-									<MenuItem onClick={sortingHandler} id={'views'} disableRipple>
-										Views
-									</MenuItem>
-								</Menu>
-							</div>
-						</Box>
-					</Stack>
-					<Stack className={'card-wrap'}>
-						{agents?.length === 0 ? (
-							<div className={'no-data'}>
-								<img src="/img/icons/icoAlert.svg" alt="" />
-								<p>No Agents found!</p>
-							</div>
-						) : (
-							agents.map((agent: Member) => {
-								return <AgentCard agent={agent} key={agent._id} />;
-							})
-						)}
-					</Stack>
-					<Stack className={'pagination'}>
-						<Stack className="pagination-box">
-							{agents.length !== 0 && Math.ceil(total / searchFilter.limit) > 1 && (
-								<Stack className="pagination-box">
-									<Pagination
-										page={currentPage}
-										count={Math.ceil(total / searchFilter.limit)}
-										onChange={paginationChangeHandler}
-										shape="circular"
-										color="primary"
-									/>
-								</Stack>
-							)}
-						</Stack>
+  const sortingHandler = (e: React.MouseEvent<HTMLLIElement>) => {
+    switch (e.currentTarget.id) {
+      case 'recent':
+        setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'DESC' });
+        setFilterSortName('Recent');
+        break;
+      case 'old':
+        setSearchFilter({ ...searchFilter, sort: 'createdAt', direction: 'ASC' });
+        setFilterSortName('Oldest order');
+        break;
+      case 'likes':
+        setSearchFilter({ ...searchFilter, sort: 'memberLikes', direction: 'DESC' });
+        setFilterSortName('Likes');
+        break;
+      case 'views':
+        setSearchFilter({ ...searchFilter, sort: 'memberViews', direction: 'DESC' });
+        setFilterSortName('Views');
+        break;
+    }
+    setSortingOpen(false);
+    setAnchorEl2(null);
+  };
 
-						{agents.length !== 0 && (
-							<span>
-								Total {total} agent{total > 1 ? 's' : ''} available
-							</span>
-						)}
-					</Stack>
-				</Stack>
-			</Stack>
-		);
-	}
+  const paginationChangeHandler = async (event: ChangeEvent<unknown>, value: number) => {
+    searchFilter.page = value;
+    await router.push(`/agent?input=${JSON.stringify(searchFilter)}`, `/agent?input=${JSON.stringify(searchFilter)}`, {
+      scroll: false,
+    });
+    setCurrentPage(value);
+  };
+
+  const likeMemberHandler = async (user: any, id: string) => {
+    try {
+      if (!id) return;
+      if (!user._id) throw new Error(Messages.error2);
+
+      //execute likePropertyHandler
+      await likeTargetMember({ variables: { input: id } });
+
+      // execute getPropertiesRefetch
+      await getAgentsRefetch({ input: initialInput });
+
+      await sweetTopSmallSuccessAlert('success', 800);
+    } catch (err: any) {
+      console.log('ERROR, likePropertyHandler:', err.message);
+      sweetMixinErrorAlert(err.message).then();
+    }
+  };
+
+  if (device === 'mobile') {
+    return <h1>AGENTS PAGE MOBILE</h1>;
+  } else {
+    return (
+      <Stack className={'agent-list-page'}>
+        <Stack className={'container'}>
+          <Stack className={'filter'}>
+            <Box component={'div'} className={'left'}>
+              <input
+                type="text"
+                placeholder={'Search for an agent'}
+                value={searchText}
+                onChange={(e: any) => setSearchText(e.target.value)}
+                onKeyDown={(event: any) => {
+                  if (event.key == 'Enter') {
+                    setSearchFilter({
+                      ...searchFilter,
+                      search: { ...searchFilter.search, text: searchText },
+                    });
+                  }
+                }}
+              />
+            </Box>
+            <Box component={'div'} className={'right'}>
+              <span>Sort by</span>
+              <div>
+                <Button onClick={sortingClickHandler} endIcon={<KeyboardArrowDownRoundedIcon />}>
+                  {filterSortName}
+                </Button>
+                <Menu anchorEl={anchorEl} open={sortingOpen} onClose={sortingCloseHandler} sx={{ paddingTop: '5px' }}>
+                  <MenuItem onClick={sortingHandler} id={'recent'} disableRipple>
+                    Recent
+                  </MenuItem>
+                  <MenuItem onClick={sortingHandler} id={'old'} disableRipple>
+                    Oldest
+                  </MenuItem>
+                  <MenuItem onClick={sortingHandler} id={'likes'} disableRipple>
+                    Likes
+                  </MenuItem>
+                  <MenuItem onClick={sortingHandler} id={'views'} disableRipple>
+                    Views
+                  </MenuItem>
+                </Menu>
+              </div>
+            </Box>
+          </Stack>
+          <Stack className={'card-wrap'}>
+            {agents?.length === 0 ? (
+              <div className={'no-data'}>
+                <img src="/img/icons/icoAlert.svg" alt="" />
+                <p>No Agents found!</p>
+              </div>
+            ) : (
+              agents.map((agent: Member) => {
+                return <AgentCard agent={agent} key={agent._id} likeMemberHandler={likeMemberHandler} />;
+              })
+            )}
+          </Stack>
+          <Stack className={'pagination'}>
+            <Stack className="pagination-box">
+              {agents.length !== 0 && Math.ceil(total / searchFilter.limit) > 1 && (
+                <Stack className="pagination-box">
+                  <Pagination
+                    page={currentPage}
+                    count={Math.ceil(total / searchFilter.limit)}
+                    onChange={paginationChangeHandler}
+                    shape="circular"
+                    color="primary"
+                  />
+                </Stack>
+              )}
+            </Stack>
+
+            {agents.length !== 0 && (
+              <span>
+                Total {total} agent{total > 1 ? 's' : ''} available
+              </span>
+            )}
+          </Stack>
+        </Stack>
+      </Stack>
+    );
+  }
 };
 
 AgentList.defaultProps = {
-	initialInput: {
-		page: 1,
-		limit: 10,
-		sort: 'createdAt',
-		direction: 'DESC',
-		search: {},
-	},
+  initialInput: {
+    page: 1,
+    limit: 10,
+    sort: 'createdAt',
+    direction: 'DESC',
+    search: {},
+  },
 };
 
 export default withLayoutBasic(AgentList);
