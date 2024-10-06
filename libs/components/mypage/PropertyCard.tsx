e47d5168c8f2ef92847bf80839a4c9a14a54bd05
tttemurbek <nonemenoneme9999@gmail.com>
2024-10-06 14:50:14 +0900

    feat: develop MyProperties Sectional Component logic

--- libs/components/mypage/PropertyCard.tsx
+++ libs/components/mypage/PropertyCard.tsx
@@ -11,122 +11,122 @@ import { useRouter } from 'next/router';
 import { PropertyStatus } from '../../enums/property.enum';
 
 interface PropertyCardProps {
-	property: Property;
-	deletePropertyHandler?: any;
-	memberPage?: boolean;
-	updatePropertyHandler?: any;
+  property: Property;
+  deletePropertyHandler?: any;
+  memberPage?: boolean;
+  updatePropertyHandler?: any;
 }
 
 export const PropertyCard = (props: PropertyCardProps) => {
-	const { property, deletePropertyHandler, memberPage, updatePropertyHandler } = props;
-	const device = useDeviceDetect();
-	const router = useRouter();
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
+  const { property, deletePropertyHandler, memberPage, updatePropertyHandler } = props;
+  const device = useDeviceDetect();
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-	/** HANDLERS **/
-	const pushEditProperty = async (id: string) => {
-		console.log('+pushEditProperty: ', id);
-		await router.push({
-			pathname: '/mypage',
-			query: { category: 'addProperty', propertyId: id },
-		});
-	};
+  /** HANDLERS **/
+  const pushEditProperty = async (id: string) => {
+    console.log('+pushEditProperty: ', id);
+    await router.push({
+      pathname: '/mypage',
+      query: { category: 'addProperty', propertyId: id },
+    });
+  };
 
-	const pushPropertyDetail = async (id: string) => {
-		if (memberPage)
-			await router.push({
-				pathname: '/property/detail',
-				query: { id: id },
-			});
-		else return;
-	};
+  const pushPropertyDetail = async (id: string) => {
+    if (memberPage)
+      await router.push({
+        pathname: '/property/detail',
+        query: { id: id },
+      });
+    else return;
+  };
 
-	const handleClick = (event: any) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	if (device === 'mobile') {
-		return <div>MOBILE PROPERTY CARD</div>;
-	} else
-		return (
-			<Stack className="property-card-box">
-				<Stack className="image-box" onClick={() => pushPropertyDetail(property?._id)}>
-					<img src={`${process.env.REACT_APP_API_URL}/${property.propertyImages[0]}`} alt="" />
-				</Stack>
-				<Stack className="information-box" onClick={() => pushPropertyDetail(property?._id)}>
-					<Typography className="name">{property.propertyTitle}</Typography>
-					<Typography className="address">{property.propertyAddress}</Typography>
-					<Typography className="price">
-						<strong>${formatterStr(property?.propertyPrice)}</strong>/ mo
-					</Typography>
-				</Stack>
-				<Stack className="date-box">
-					<Typography className="date">
-						<Moment format="DD MMMM, YYYY">{property.createdAt}</Moment>
-					</Typography>
-				</Stack>
-				<Stack className="status-box">
-					<Stack className="coloured-box" sx={{ background: '#E5F0FD' }} onClick={handleClick}>
-						<Typography className="status" sx={{ color: '#3554d1' }}>
-							{property.propertyStatus}
-						</Typography>
-					</Stack>
-				</Stack>
-				{!memberPage && property.propertyStatus !== 'SOLD' && (
-					<Menu
-						anchorEl={anchorEl}
-						open={open}
-						onClose={handleClose}
-						PaperProps={{
-							elevation: 0,
-							sx: {
-								width: '70px',
-								mt: 1,
-								ml: '10px',
-								overflow: 'visible',
-								filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-							},
-							style: {
-								padding: 0,
-								display: 'flex',
-								justifyContent: 'center',
-							},
-						}}
-					>
-						{property.propertyStatus === 'ACTIVE' && (
-							<>
-								<MenuItem
-									disableRipple
-									onClick={() => {
-										handleClose();
-										updatePropertyHandler(PropertyStatus.SOLD, property?._id);
-									}}
-								>
-									Sold
-								</MenuItem>
-							</>
-						)}
-					</Menu>
-				)}
+  if (device === 'mobile') {
+    return <div>MOBILE PROPERTY CARD</div>;
+  } else
+    return (
+      <Stack className="property-card-box">
+        <Stack className="image-box" onClick={() => pushPropertyDetail(property?._id)}>
+          <img src={`${process.env.REACT_APP_API_URL}/${property.propertyImages[0]}`} alt="" />
+        </Stack>
+        <Stack className="information-box" onClick={() => pushPropertyDetail(property?._id)}>
+          <Typography className="name">{property.propertyTitle}</Typography>
+          <Typography className="address">{property.propertyAddress}</Typography>
+          <Typography className="price">
+            <strong>${formatterStr(property?.propertyPrice)}</strong>/ mo
+          </Typography>
+        </Stack>
+        <Stack className="date-box">
+          <Typography className="date">
+            <Moment format="DD MMMM, YYYY">{property.createdAt}</Moment>
+          </Typography>
+        </Stack>
+        <Stack className="status-box">
+          <Stack className="coloured-box" sx={{ background: '#E5F0FD' }} onClick={handleClick}>
+            <Typography className="status" sx={{ color: '#3554d1' }}>
+              {property.propertyStatus}
+            </Typography>
+          </Stack>
+        </Stack>
+        {!memberPage && property.propertyStatus !== 'SOLD' && (
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                width: '70px',
+                mt: 1,
+                ml: '10px',
+                overflow: 'visible',
+                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              },
+              style: {
+                padding: 0,
+                display: 'flex',
+                justifyContent: 'center',
+              },
+            }}
+          >
+            {property.propertyStatus === 'ACTIVE' && (
+              <>
+                <MenuItem
+                  disableRipple
+                  onClick={() => {
+                    handleClose();
+                    updatePropertyHandler(PropertyStatus.SOLD, property?._id);
+                  }}
+                >
+                  Sold
+                </MenuItem>
+              </>
+            )}
+          </Menu>
+        )}
 
-				<Stack className="views-box">
-					<Typography className="views">{property.propertyViews.toLocaleString()}</Typography>
-				</Stack>
-				{!memberPage && (
-					<Stack className="action-box">
-						<IconButton className="icon-button" onClick={() => pushEditProperty(property._id)}>
-							<ModeIcon className="buttons" />
-						</IconButton>
-						<IconButton className="icon-button" onClick={() => deletePropertyHandler(property._id)}>
-							<DeleteIcon className="buttons" />
-						</IconButton>
-					</Stack>
-				)}
-			</Stack>
-		);
+        <Stack className="views-box">
+          <Typography className="views">{property.propertyViews.toLocaleString()}</Typography>
+        </Stack>
+        {!memberPage && property.propertyStatus === PropertyStatus.ACTIVE && (
+          <Stack className="action-box">
+            <IconButton className="icon-button" onClick={() => pushEditProperty(property._id)}>
+              <ModeIcon className="buttons" />
+            </IconButton>
+            <IconButton className="icon-button" onClick={() => deletePropertyHandler(property._id)}>
+              <DeleteIcon className="buttons" />
+            </IconButton>
+          </Stack>
+        )}
+      </Stack>
+    );
 };
